01388c660e10699432f5dd8d82b798138c0a64a8
amimaro <amir.zahlan@gmail.com>
2022-03-10 17:08:16 -0300

    Update decimals calculator Convert to usd and brl

--- app/components/AppInput.tsx
+++ app/components/AppInput.tsx
@@ -32,6 +32,7 @@ export default function AppInput({
       )}
       <input
         id={id}
+        name={id}
         className={`border-2 w-full p-2 focus:ring-4 rounded-md ${className}`}
         type={type ?? "text"}
         value={value}
@@ -39,6 +40,7 @@ export default function AppInput({
         onChange={onChange}
         readOnly={readOnly}
         disabled={disabled}
+        autoComplete="off"
       />
     </div>
   );
--- app/routes/decimals.tsx
+++ app/routes/decimals.tsx
@@ -1,21 +1,69 @@
-import { ethers } from "ethers";
-import { useState } from "react";
+import { utils } from "ethers";
+
+import { useRef, useState } from "react";
+import {
+  ActionFunction,
+  Form,
+  useActionData,
+  useSubmit,
+  useTransition,
+} from "remix";
 import AppCard from "~/components/AppCard";
 import AppInput from "~/components/AppInput";
+import useDebounce from "~/hooks/useDebounce";
+
+type ActionData = {
+  formError?: string;
+  usd: string;
+  brl: string;
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const ether = formData.get("ether");
+  const parsedEther = parseFloat(ether as string);
+
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/simple/price?ids=ethereum,bitcoin&vs_currencies=usd,brl"
+  );
+  const market = await res.json();
+
+  if (isNaN(parsedEther)) {
+    return {
+      usd: "",
+      brl: "",
+    };
+  }
+
+  return {
+    usd: (parseFloat(ether as string) * market.ethereum.usd).toFixed(4),
+    brl: (parseFloat(ether as string) * market.ethereum.brl).toFixed(4),
+  };
+};
 
 export default function DecimalsPage() {
+  const actionData = useActionData<ActionData>();
+  const transition = useTransition();
+  const submit = useSubmit();
+  const form = useRef();
+
   const [wei, setWei] = useState("");
   const [gwei, setGwei] = useState("");
   const [ether, setEther] = useState("");
 
-  const handleChange = (unit: string, e: React.ChangeEvent<any>) => {
+  useDebounce(1000, ether, () =>
+    submit(form.current as any, { replace: true })
+  );
+
+  const handleInputChange = (unit: string, e: React.ChangeEvent<any>) => {
     const value = e.target.value;
     const re = /^-?\d*\.?\d*$/;
-    const isValidNumber = re.test(value);
+    const reWei = /^-?\d*$/;
+    const isValidNumber =
+      unit === "wei" ? reWei.test(e.target.value) : re.test(e.target.value);
 
     if (!isValidNumber) return;
-
-    if (value === "") {
+    if (value === "" || isNaN(parseFloat(value))) {
       setWei("");
       setGwei("");
       setEther("");
@@ -24,52 +72,84 @@ export default function DecimalsPage() {
 
     if (unit === "wei") {
       setWei(value);
-      setEther(ethers.utils.formatUnits(value, "ether"));
-      setGwei(ethers.utils.formatUnits(value, "gwei"));
+      setGwei(utils.formatUnits(value, "gwei"));
+      setEther(utils.formatUnits(value, "ether"));
     }
 
     if (unit === "gwei") {
       setGwei(value);
-      setWei(ethers.utils.formatUnits(value, "wei"));
-      setEther(ethers.utils.formatUnits(value, "ether"));
+      const weiVal = parseFloat(value) * 10 ** 9 + "";
+      setWei(weiVal);
+      setEther(utils.formatUnits(weiVal, "ether"));
     }
 
     if (unit === "ether") {
       setEther(value);
-      setWei(ethers.utils.formatUnits(value, "wei"));
-      setGwei(ethers.utils.formatUnits(value, "gwei"));
+      const weiVal = utils.parseUnits(value).toString();
+      setWei(weiVal);
+      setGwei(utils.formatUnits(weiVal, "gwei"));
     }
   };
 
   return (
-    <div className="md:w-96 mx-auto">
+    <div className="md:w-96 mx-auto pt-10">
       <AppCard>
-        <div className="flex flex-col gap-4 w-full">
-          <div className="flex items-center gap-4">
-            <div className="w-20 font-semibold">Wei</div>
-            <AppInput
-              value={wei}
-              onChange={(e) => handleChange("wei", e)}
-              placeholder="Wei value"
-            />
+        <Form method="post" ref={form as any}>
+          <div className="flex flex-col gap-4 w-full">
+            <div className="flex items-center gap-4">
+              <div className="w-20 font-semibold">Wei</div>
+              <AppInput
+                id="wei"
+                value={wei}
+                onChange={(e) => handleInputChange("wei", e)}
+                placeholder="Wei value"
+              />
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="w-20 font-semibold">Gwei</div>
+              <AppInput
+                id="gwei"
+                value={gwei}
+                onChange={(e) => handleInputChange("gwei", e)}
+                placeholder="Gwei value"
+              />
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="w-20 font-semibold">Ether</div>
+              <AppInput
+                id="ether"
+                value={ether}
+                onChange={(e) => handleInputChange("ether", e)}
+                placeholder="Ether value"
+              />
+            </div>
+            {actionData &&
+              actionData.usd.length > 0 &&
+              transition.state !== "submitting" &&
+              ether.length > 0 && (
+                <>
+                  <div className="flex items-center gap-4">
+                    <div className="w-20 font-semibold">USD</div>
+                    <AppInput
+                      value={actionData?.usd}
+                      onChange={() => {}}
+                      placeholder="USD value"
+                      readOnly
+                    />
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <div className="w-20 font-semibold">BRL</div>
+                    <AppInput
+                      value={actionData?.brl}
+                      onChange={() => {}}
+                      placeholder="USD value"
+                      readOnly
+                    />
+                  </div>
+                </>
+              )}
           </div>
-          <div className="flex items-center gap-4">
-            <div className="w-20 font-semibold">Gwei</div>
-            <AppInput
-              value={gwei}
-              onChange={(e) => handleChange("gwei", e)}
-              placeholder="Gwei value"
-            />
-          </div>
-          <div className="flex items-center gap-4">
-            <div className="w-20 font-semibold">Ether</div>
-            <AppInput
-              value={ether}
-              onChange={(e) => handleChange("ether", e)}
-              placeholder="Ether value"
-            />
-          </div>
-        </div>
+        </Form>
       </AppCard>
     </div>
   );
